20b2c5e2a5c23b96707111c35472bb00b3824da4
doceallaigh <dylan.oceallaigh@gmail.com>
2017-09-06 21:49:42 -0400

    Implemented basic player movement
    Added basic player movement with translation on thrust an strafe axes and rotation on yaw.

---
+++ SkyOnFire/Assets/IForceProvider.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public interface IForceProvider
+{
+    Vector3 GetTranslationalForce();
+
+    Vector3 GetRotationalForce();
+}
--- SkyOnFire/Assets/PlayerScript.cs
+++ SkyOnFire/Assets/PlayerScript.cs
@@ -1,15 +1,60 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using UnityEngine;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour
+{
+    public ForceProviderScript forceProvider;
 
-	// Use this for initialization
-	void Start () {
+    public Vector3 translationalVelocity;
+    public Vector3 rotationalVelocity;
+
+    // Use this for initialization
+    public void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	public void Update ()
+	{
+	    float deltaTime = Time.deltaTime;
+
+	    this.ApplyTranslation(deltaTime);
+	    this.ApplyRotation(deltaTime);
+	    this.ApplyTranslationalForce(deltaTime);
+	    this.ApplyRotationalForce(deltaTime);
 	}
+
+    private void ApplyTranslation(float deltaTime)
+    {
+        this.transform.Translate(this.translationalVelocity * deltaTime, Space.World);
+    }
+
+    private void ApplyRotation(float deltaTime)
+    {
+        this.transform.Rotate(this.rotationalVelocity * deltaTime);
+    }
+
+    private void ApplyTranslationalForce(float deltaTime)
+    {
+        //TODO
+        const int mass = 1;
+
+        Vector3 translationalForce = this.forceProvider.GetTranslationalForce();
+        Vector3 translationalAcceleration = translationalForce / mass;
+
+        Vector3 translationalMoment = translationalAcceleration * deltaTime;
+        this.translationalVelocity += translationalMoment;
+    }
+
+    private void ApplyRotationalForce(float deltaTime)
+    {
+        //TODO
+        const int mass = 1;
+
+        Vector3 rotationalForce = this.forceProvider.GetRotationalForce();
+        Vector3 rotationalAcceleration = rotationalForce / mass;
+
+        Vector3 rotationalMoment = rotationalAcceleration * deltaTime;
+        this.rotationalVelocity += rotationalMoment;
+    }
 }
