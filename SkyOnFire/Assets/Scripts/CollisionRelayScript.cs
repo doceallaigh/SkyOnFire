998c521338ba90b99af72e140199fb539f3277f6
Unknown <dylan.oceallaigh@gmail.com>
2017-09-10 15:23:08 -0400

    Added projectile spawning for enemy
    Added projectile spawning for enemy; Revised enemy layout to fire in correct direction and handle child collision

---
+++ SkyOnFire/Assets/Scripts/CollisionRelayScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionRelayScript : MonoBehaviour
+{
+    public class CollisionEventArgs : EventArgs
+    {
+        public CollisionEventArgs(Collider collider)
+        {
+            this.Collider = collider;
+        }
+
+        public Collider Collider { get; private set; }
+    }
+
+    public EventHandler<CollisionEventArgs> CollisionEvent { get; set; }
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.CollisionEvent != null)
+        {
+            CollisionEventArgs collisionEventArgs = new CollisionEventArgs(other);
+            this.CollisionEvent.Invoke(this, collisionEventArgs);
+        }
+    }
+}
--- SkyOnFire/Assets/Scripts/EnemyScript.cs
+++ SkyOnFire/Assets/Scripts/EnemyScript.cs
@@ -1,23 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyScript : MonoBehaviour {
+public class EnemyScript : MonoBehaviour
+{
+    public ProjectileSpawnerScript projectileSpawner;
+    public CollisionRelayScript collisionRelayScript;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    this.collisionRelayScript.CollisionEvent += this.HandleCollisionRelay;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        this.SpawnProjectiles();
 	}
+
+    private void SpawnProjectiles()
+    {
+        this.projectileSpawner.TrySpawn();
+    }
+
+    private void HandleCollisionRelay(object sender, CollisionRelayScript.CollisionEventArgs eventArgs)
+    {
+        this.OnTriggerEnter(eventArgs.Collider);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("yeehaw");
 
-        if(other.name == "PlayerShip")
+        if (other.name == "PlayerShip")
         {
             Debug.Log("buh");
         }
