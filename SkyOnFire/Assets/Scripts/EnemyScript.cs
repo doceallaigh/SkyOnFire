1c653cf19c4b82bc5e650839f7bea48c604e1414
Unknown <dylan.oceallaigh@gmail.com>
2017-09-10 19:47:39 -0400

    Added basic tracking for the enemy ship

--- SkyOnFire/Assets/Scripts/EnemyScript.cs
+++ SkyOnFire/Assets/Scripts/EnemyScript.cs
@@ -3,19 +3,65 @@ using System.Collections;
 
 public class EnemyScript : MonoBehaviour
 {
+    public ForceProviderScript forceProvider;
     public ProjectileSpawnerScript projectileSpawner;
     public CollisionRelayScript collisionRelayScript;
 
-	// Use this for initialization
-	void Start ()
+    public Vector3 translationalVelocity;
+    public Vector3 rotationalVelocity;
+
+    // Use this for initialization
+    void Start ()
 	{
 	    this.collisionRelayScript.CollisionEvent += this.HandleCollisionRelay;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        this.SpawnProjectiles();
-	}
+	void Update ()
+    {
+	    float deltaTime = Time.deltaTime;
+
+	    this.ApplyTranslation(deltaTime);
+	    this.ApplyRotation(deltaTime);
+	    this.ApplyTranslationalForce(deltaTime);
+	    this.ApplyRotationalForce(deltaTime);
+
+	    this.SpawnProjectiles();
+    }
+
+    private void ApplyTranslation(float deltaTime)
+    {
+        this.transform.Translate(this.translationalVelocity * deltaTime, Space.World);
+    }
+
+    private void ApplyRotation(float deltaTime)
+    {
+        this.transform.Rotate(this.rotationalVelocity * deltaTime);
+    }
+
+    private void ApplyTranslationalForce(float deltaTime)
+    {
+        //TODO
+        const int mass = 1;
+
+        Vector3 translationalForce = this.forceProvider.GetTranslationalForce();
+        Vector3 translationalAcceleration = translationalForce / mass;
+
+        Vector3 translationalMoment = translationalAcceleration * deltaTime;
+        this.translationalVelocity += translationalMoment;
+    }
+
+    private void ApplyRotationalForce(float deltaTime)
+    {
+        //TODO
+        const int mass = 1;
+
+        Vector3 rotationalForce = this.forceProvider.GetRotationalForce();
+        Vector3 rotationalAcceleration = rotationalForce / mass;
+
+        Vector3 rotationalMoment = rotationalAcceleration * deltaTime;
+        this.rotationalVelocity += rotationalMoment;
+    }
 
     private void SpawnProjectiles()
     {
---
+++ SkyOnFire/Assets/Scripts/TargetTrackingForceProvider.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetTrackingForceProvider : ForceProviderScript
+{
+    public Transform target;
+    public Vector3 forceLimit;
+    public float rotationAxisMultiplier;
+
+    private Vector3 rotationalForce;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+
+        this.UpdateRotationalForce(deltaTime);
+    }
+
+    public override Vector3 GetTranslationalForce()
+    {
+        return Vector3.zero;
+    }
+
+    public override Vector3 GetRotationalForce()
+    {
+        return this.rotationalForce;
+    }
+
+    private void UpdateRotationalForce(float deltaTime)
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(this.target.position - this.transform.position, this.transform.up);
+
+        // TODO This should be done in a less naive way
+        this.rotationalForce = new Vector3(
+            targetRotation.eulerAngles.x - this.transform.rotation.eulerAngles.x, 
+            targetRotation.eulerAngles.y - this.transform.rotation.eulerAngles.y, 
+            targetRotation.eulerAngles.z - this.transform.rotation.eulerAngles.z);
+        this.rotationalForce *= this.rotationAxisMultiplier;
+
+        this.rotationalForce = Vector3.Min(this.rotationalForce, this.forceLimit);
+        this.rotationalForce = Vector3.Max(this.rotationalForce, -this.forceLimit);
+        this.rotationalForce *= deltaTime;
+    }
+}
