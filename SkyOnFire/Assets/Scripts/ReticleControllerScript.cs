97cea495c2ff74f8632286a8b6cc97891e48079b
Unknown <dylan.oceallaigh@gmail.com>
2017-09-11 00:39:25 -0400

    Added basic reticle

---
+++ SkyOnFire/Assets/Scripts/ReticleControllerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ReticleControllerScript : MonoBehaviour
+{
+    public Camera camera;
+    public GUITexture reticleTexture;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float normalizedX = Input.mousePosition.x / this.camera.pixelWidth;
+	    float normalizedY = Input.mousePosition.y / this.camera.pixelHeight;
+
+        Vector2 normalizedmousePosition = new Vector2(normalizedX, normalizedY);
+
+        this.reticleTexture.transform.position = normalizedmousePosition;
+	}
+}
