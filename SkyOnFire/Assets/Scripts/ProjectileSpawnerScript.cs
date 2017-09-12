c50e2d778583b5d77b4e739be354a4846510b39c
Unknown <dylan.oceallaigh@gmail.com>
2017-09-11 20:53:02 -0400

    Implemented basic aim

--- SkyOnFire/Assets/Scripts/ProjectileSpawnerScript.cs
+++ SkyOnFire/Assets/Scripts/ProjectileSpawnerScript.cs
@@ -1,24 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class ProjectileSpawnerScript : EntitySpawnerScript
 {
     public int projectileSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Vector3 aimDirection;
+
+    // Use this for initialization
+    void Start ()
+    {
+        this.aimDirection = this.transform.forward;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
+    public void Aim(Vector3 shotDirection)
+    {
+        this.aimDirection = shotDirection;
+    }
+
     protected override void Spawn()
     {
         GameObject projectileGameObject = GameObject.Instantiate(Resources.Load("Projectile")) as GameObject;
         ProjectileScript projectileScript = projectileGameObject.GetComponent<ProjectileScript>();
         projectileScript.transform.position = this.transform.position;
-        projectileScript.velocity = this.transform.forward.normalized * this.projectileSpeed;
+        projectileScript.velocity = this.aimDirection.normalized * this.projectileSpeed;
     }
 }
